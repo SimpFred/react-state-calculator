33c1f031d58be065f7e3fe49ab6c193057aee644
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-09-25 13:23:25 +0200

    Refactor calculator app to use state and add functionality for storing and recalling values

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,120 @@
-import "./App.css"
+import { useState, useEffect } from "react";
+import "./App.css";
+import Panel from "../src/components/Panel";
+import Display from "../src/components/Display";
+import Button from "../src/components/Button";
+
+const numberButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "Clear"];
+const operatorButtons = ["+", "-", "*", "÷"];
 
 function App() {
+  const [panel1Value, setPanel1Value] = useState(0);
+  const [panel2Value, setPanel2Value] = useState("+");
+  const [panel3Value, setPanel3Value] = useState(0);
+  const [result, setResult] = useState(0);
+  const [clearIsPressed, setClearIsPressed] = useState(false);
+  const [storedValue, setStoredValue] = useState(null);
+
+  useEffect(() => {
+    calculateResult();
+    setClearIsPressed(false);
+  }, [clearIsPressed]);
+
+  const handleButtonClick = (label, panel) => {
+    if (label === "Clear") {
+      if (panel === 1) {
+        setPanel1Value(0);
+        setClearIsPressed(true);
+      }
+      if (panel === 3) {
+        setPanel3Value(0);
+        setClearIsPressed(true);
+      }
+    } else {
+      if (panel === 1) {
+        setPanel1Value((prev) =>
+          prev === 0 ? label : parseInt(prev.toString() + label)
+        );
+      }
+      if (panel === 2) {
+        setPanel2Value(label);
+      }
+      if (panel === 3) {
+        setPanel3Value((prev) =>
+          prev === 0 ? parseInt(label) : parseInt(prev.toString() + label)
+        );
+      }
+    }
+  };
+
+  const calculateResult = () => {
+    let res = 0;
+    switch (panel2Value) {
+      case "+":
+        res = panel1Value + panel3Value;
+        break;
+      case "-":
+        res = panel1Value - panel3Value;
+        break;
+      case "*":
+        res = panel1Value * panel3Value;
+        break;
+      case "÷":
+        res = panel1Value / panel3Value;
+        break;
+      default:
+        break;
+    }
+    setResult(res);
+  };
+
+  const handleStore = () => {
+    setStoredValue(result);
+  };
+
+  const handleRecall = (panel) => {
+    if (storedValue !== null) {
+      if (panel === 1) {
+        setPanel1Value(storedValue);
+      }
+      if (panel === 3) {
+        setPanel3Value(storedValue);
+      }
+    }
+  };
 
   return (
     <div className="calculator">
-      <div className="panel">
-        <p>0</p>
-        <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
-        </div>
-      </div>
-
-      <div className="panel">
-        <p>+</p>
-        <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
-        </div>
-      </div>
-
-      <div className="panel">
-        <p>0</p>
-        <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
-        </div>
-      </div>
+      <Panel
+        displayValue={panel1Value}
+        buttons={numberButtons}
+        onButtonClick={(label) => handleButtonClick(label, 1)}
+        recallButton={"Recall"}
+        handleRecall={() => handleRecall(1)
+        }
+      />
+      <Panel
+        displayValue={panel2Value}
+        buttons={operatorButtons}
+        onButtonClick={(label) => handleButtonClick(label, 2)}
+      />
+      <Panel
+        displayValue={panel3Value}
+        buttons={numberButtons}
+        onButtonClick={(label) => handleButtonClick(label, 3)}
+        recallButton={"Recall"}
+        handleRecall={() => handleRecall(3)
+        }
+      />
       <div className="panel answer">
-        <p>0</p>
+        <Display value={result} />
         <div>
-          <button>=</button>
+          <Button label="=" onClick={calculateResult} />
+          <Button label="Store" onClick={handleStore} />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
